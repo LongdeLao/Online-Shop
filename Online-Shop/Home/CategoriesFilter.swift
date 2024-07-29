d5b18f74484f244a0211e889c2f2dadeb8d5a873
Longde Lao <longdelao@outlook.de>
2024-07-29 19:41:28 +0800

    added filter functionality

---
+++ Online-Shop/Home/CategoriesFilter.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesFilter.swift
+//  Online-Shop
+//
+//  Created by Longde Lao on 29.07.24.
+//
+
+import SwiftUI
+
+struct CategoriesFilter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesFilter()
+}
